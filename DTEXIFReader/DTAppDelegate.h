a0d719669f59de29fc7623ffacb4efaa485925bf
Darktt <pippipig@gmail.com>
2014-05-01 10:48:17 +0800

    Initial Commit

---
+++ DTEXIFReader/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  DTEXIFReader
+//
+//  Created by Darktt on 5/1/14.
+//  Copyright (c) 2014 Darktt Personal Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DTEXIFReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DTEXIFReader
+//
+//  Created by Darktt on 5/1/14.
+//  Copyright (c) 2014 Darktt Personal Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
